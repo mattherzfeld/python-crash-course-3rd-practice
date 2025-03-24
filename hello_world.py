e9d9442d77361c3855b958c0935b18ee493ccb1d
mattherzfeld <matthew.herzfeld@gmail.com>
2025-03-24 13:44:58 -0500

    Create hello_world.py
    Added a few Python basics

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#Testing out Python basics
+
+#The first program
+print("Hello Python world!")
+
+#Adding a variable
+message = "Hello Python world!"
+print(message)
+
+#Modifying existing variable
+message = "Hello Python Crash Course world!"
+print(message)
+
