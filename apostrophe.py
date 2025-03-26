f8cfb33ef408e74e109c94605314e2676207ad55
mattherzfeld <matthew.herzfeld@gmail.com>
2025-03-25 23:57:18 -0500

    Update apostrophe.py
    Practice exercises part 1

--- apostrophe.py
+++ apostrophe.py
@@ -6,3 +6,26 @@ print(message)
 #This will error (so use double quotes just to make it easier):
 #message = 'One of Python's strengths is its diverse community.'
 
+#2-3 Personal Message
+x = "Eric"
+message = f"Hello {x}, would you like to learn some Python today?"
+print(message)
+
+#2-4 Name Cases
+y = "Denise"
+print(y.lower())
+print(y.upper())
+print(y.title())
+
+#2-5 Famous Quote
+print("Albert Einstein once said, \"A person who never made a mistake never never tried anything new.\"")
+
+#2-6 Famouse Quote 2
+famous_person = "Matt Herzfeld"
+print(f"As said by {famous_person}, \"Wherever you go, there you are.\"")
+
+#2-7 Stripping Names
+
+
+
+#2-8 File Extension
