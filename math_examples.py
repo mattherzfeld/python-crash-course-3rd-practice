9b1223f3430ec489cb6330dc7656c4340ab333ac
mattherzfeld <matthew.herzfeld@gmail.com>
2025-03-27 23:57:57 -0500

    Create math_examples.py
    Math file with basic examples using math in Python.

---
+++ math_examples.py
@@ -0,0 +1,38 @@
+#Doing math with Python
+#-----------------------
+
+# Using Integers
+#Addition - Answer: 8
+print(3+5)
+#Subtraction - Answer: 3
+print(7-4)
+#Multiplication - Answer: 24
+print(8*3)
+#Division - Answer: 2
+print(8/4)
+#Exponents - Answer: 27
+print(3**3)
+#Expressions - Answer: 10
+print((2+3)*2)
+#Modulus - this provides the remainder from using division - Answer: 3
+print(7%4)
+
+# Using Floaters
+#Basic arithmetic with floaters
+print(0.1+0.2)
+print(1.5-0.3)
+print((1.5*2.1))
+
+# Using underscores
+universe_age = 14_000_000_000
+print(universe_age)
+
+# Multiple Assignment
+x, y, z = 0, 1, 2
+
+# Constants
+MAX_CONNECTIONS = 5_000
+
+# The Zen of Python
+import this
+print(this)
