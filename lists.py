f584a512978b1950aae59ff447d25fb654fc56e1
mattherzfeld <matthew.herzfeld@gmail.com>
2025-03-30 00:34:14 -0500

    Update lists.py
    Completed exercises 3-4 thru 3-7

--- lists.py
+++ lists.py
@@ -44,4 +44,50 @@ print(motorcycles)
 print(popped_motorcycle)
 
 last_owned = motorcycles.pop()
-print(f"The last motorcycle I owned was a {last_owned.title()}")
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+del(motorcycles[0])
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+
+too_expensive = "ducati"
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+#3-4 Guest List
+guestList = ["Tiger Woods", "Vladimir Lenin", "Otto von Bismarck"]
+print(f"Dear {guestList[0].title()}, please come to my dinner party.")
+print(f"Dear Comrade {guestList[1].title()}, you are cordially invited to my dinner party.")
+print(f"Dear Mr. {guestList[2].title()}, you are invited to my dinner party. Please come.")
+
+#3-5 Changing Guest List
+cannot_make_it = "Otto von Bismarck"
+guestList.remove(cannot_make_it)
+print(f"\nUnfortunately, {cannot_make_it.title()} can no longer attend the dinner party.")
+guestList.append("Theodore Roosevelt")
+print(f"Dear {guestList[2].title()}, please consider attending my dinner party. Thank you!")
+print(guestList)
+
+#3-6 More Guests
+guestList.append("Bill Walton")
+guestList.insert(0, "Steven Spielberg")
+guestList.insert(2, "Vincent Price")
+print(guestList)
+print(f"Dear Guests, a new table has become available and now we will be a party of 7")
+
+#3-7 Shrinking Guest List
+    #I'm going to cheat and use a while loop to shorten my codebase:
+while len(guestList) > 2:
+    poppedGuest = guestList.pop()
+    print(f"I'm sorry, {poppedGuest.title()}. I can no longer extend an invite to the dinner party. My apologies.")
+
+print(guestList)
+print(f"Hi {guestList[0].title()}, I am pleased to inform you are still invited to the dinner party.")
+print(f"Hi {guestList[1].title()}, I am pleased to inform you are still invited to the dinner party.")
+
+#Clear out the list and print to double check
+del(guestList[:])
+print(guestList)
