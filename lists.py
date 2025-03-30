0731821dc72af5f6a586139447387c05fda39a9e
mattherzfeld <matthew.herzfeld@gmail.com>
2025-03-29 23:55:14 -0500

    Update lists.py
    Adding/Removing items to a list.

--- lists.py
+++ lists.py
@@ -21,3 +21,27 @@ print(f"The names list has {count} items in it.")
 #3-3 Your Own List
 cars = ["Honda", "Ford", "Dodge", "Nissan", "Toyota", "Hyundai", "Chevrolet"]
 print(f"I'd like to own a {cars[1].upper()}!!")
+
+#Modifying Lists
+motorcycles = ["Honda", "Yamaha", "Suzuki"]
+motorcycles[0] = "Ducati"
+print(motorcycles)
+
+motorcycles.append("Honda")
+print(motorcycles)
+
+#Two parameters (index, item being added) - does not overwrite "Ducati that was in that index, just moves items back"
+motorcycles.insert(0, "Harley-Davidson")
+print(motorcycles)
+
+#Deletes indexed item
+del(motorcycles[0])
+print(motorcycles)
+
+#Removing using pop() method
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+last_owned = motorcycles.pop()
+print(f"The last motorcycle I owned was a {last_owned.title()}")
