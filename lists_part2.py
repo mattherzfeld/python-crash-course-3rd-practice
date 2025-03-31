3ac6f1cdddedc1fce4ef2a7476f336cfa5702ded
mattherzfeld <matthew.herzfeld@gmail.com>
2025-03-31 00:09:45 -0500

    Update lists_part2.py
    Using ranges with functions

--- lists_part2.py
+++ lists_part2.py
@@ -7,3 +7,28 @@ for magician in magicians:
 
 print("Thank you everyone. That was a great magic show!")
 
+#4-1 Pizzas
+pizzas = ["pepperoni", "sausage", "mushroom"]
+
+for pizza in pizzas:
+    print(f"We have a {pizza} pizza on the menu.")
+
+print("\nI really love pizza!")
+
+#4-2 Animals
+animals = ["dog", "cat", "bird"]
+for animal in animals:
+    print(f"A {animal} would make a really great pet!")
+
+print("These are all animals that are located at the local pet shop.")
+
+#Using the range() function
+for value in range(1, 5): #notice how the second parameter is not included in the actual list
+    print(value)
+
+for value in range(6): #if only one parameter used, defaults to starting list at zero
+    print(value)
+
+#Using range() to make a list of numbers
+numbers = list(range(1,6))
+print(numbers)
