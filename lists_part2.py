e124a32bccc46eaab6db8d1baa3c14c2c7642169
mattherzfeld <matthew.herzfeld@gmail.com>
2025-03-31 14:13:57 -0500

    Update lists_part2.py
    plus lists and for loops

--- lists_part2.py
+++ lists_part2.py
@@ -32,3 +32,81 @@ for value in range(6): #if only one parameter used, defaults to starting list at
 #Using range() to make a list of numbers
 numbers = list(range(1,6))
 print(numbers)
+
+#even_numbers
+even_numbers = list(range(2,11,2)) #we can pass a third argument which is a "step" indicator
+print(even_numbers)
+
+#square_numbers
+squares = []
+for value in range(1,11):   #list loops from ints 1 to 10
+    square = value ** 2     #set square variable to listed number times itself
+    squares.append(square)  #add to end of squares list 
+
+print(squares)
+
+#more conscise square_numbers
+squares1 = []
+for value in range(1,11):
+    squares1.append(value ** 2)
+
+print(squares1)
+
+#simple statistics with a list of ints
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+#list comprehensions (allows you generate code in ONE line)
+squares = [value ** 2 for value in range(1, 11)]
+print(squares)
+
+#4-3 Counting to Twenty
+one_to_twenty = [value + 1 for value in range(0, 20) ]
+print(one_to_twenty)
+
+#4-4 One Million
+#one_to_million = [value + 1 for value in range(0, 999_999)]
+#print(one_to_million)
+
+#4-5 Summing a Million
+one_to_million = [value + 1 for value in range(0, 999_999)]
+print(min(one_to_million))
+print(max(one_to_million))
+print(sum(one_to_million))
+
+#4-6 Odd Numbers
+odd_numbers = list(range(1, 20, 2))
+for value in odd_numbers:
+    print(value)
+
+#4-7 Threes
+multiples_of_three = list(range(3, 31, 3))
+for value in multiples_of_three:
+    print(value)
+
+#4-8 Cubes
+cubes = []
+for value in range(1, 11):
+    cubed = value ** 3
+    cubes.append(cubed)
+
+print(cubes)
+
+#4-9 Cube Comprehension
+cubes = [value ** 3 for value in range(1, 11)]
+print(cubes)
+
+#Slicing a List
+players = ["charles", "martina", "michael", "florence", "eli"]
+print(players[0:3]) #starts at index zero and stops at index 3(omits 3, does not include)
+print(players[1:4])
+print(players[:4])  #starts at index zero by default
+print(players[2:])  #ends at final index (includes all)
+print(players[-3:]) #prints the last three items in the list
+
+#Looping thru a List
+print("\nHere are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
