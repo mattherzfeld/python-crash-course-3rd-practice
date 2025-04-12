2b51053e72986f3646dc3cf55d858f63bc3afaa1
mattherzfeld <matthew.herzfeld@gmail.com>
2025-04-12 01:18:40 -0500

    Update if_statements.py
    if-elif-else chain

--- if_statements.py
+++ if_statements.py
@@ -43,5 +43,71 @@ age_0 = 22
 age_1 = 18
 print(age_0 >= 21 or age_1 >= 21) #prints true due to age_0 being greater than or equal to 21
 
+# using the keyword "in"
+request_toppings = ["mushroom", "onions", "pineapple"]
+
+print("mushroom" in request_toppings) #prints True due to the the inquired topping being in the list
+print("pepperoni" in request_toppings) #prints False ""
+
+# checking whether something is NOT in a list
+banned_users = ["andrew", "carolina", "david"]
+user = "marie"
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.") # not in keyword
+
+# Boolean expressions
+game_active = True
+can_edit = False
+
+# if statements
+age = 18
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else: 
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18.")
+
+# if-elif-else chain
+
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost if $40.")
+
+# more efficient version:
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+
+print(f"You admission cost is ${price}.")
+
+# multiple elif blocks
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20 #effectively a senior discount
+
+print(f"Your admission cost is ${price}.")
+
+
 
 
