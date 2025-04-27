976fd11b6896afcde293a645dba8208960daa88d
mattherzfeld <matthew.herzfeld@gmail.com>
2025-04-26 23:38:30 -0500

    Update functions.py
    More function basics, plus exercises

--- functions.py
+++ functions.py
@@ -11,3 +11,17 @@ def greet_user(username):
     print(f"Hello, {username.title()}!")
 
 greet_user("jesse")
+
+# 8-1
+
+def display_message():
+    print("I am learning about functions in this chapter.")
+
+display_message()
+
+# 8-2
+
+def favorite_book(book_title): #function name with optional parameter - in this case, "book_title"
+    print(f"One of my favorite books is {book_title.title()}.")
+
+favorite_book("Alice in Wonderland")
