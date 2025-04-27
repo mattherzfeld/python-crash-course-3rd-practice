7a93947cdc0cb98731ec88333ada468c2ea3da36
mattherzfeld <matthew.herzfeld@gmail.com>
2025-04-26 23:52:54 -0500

    Update functions.py
    positional/keyword arguments with functions

--- functions.py
+++ functions.py
@@ -25,3 +25,28 @@ def favorite_book(book_title): #function name with optional parameter - in this 
     print(f"One of my favorite books is {book_title.title()}.")
 
 favorite_book("Alice in Wonderland")
+
+# Positional Arguments
+
+"""The simplest way to  
+    do this is based on the order of the arguments provided. Values matched 
+    up this way are called positional arguments."""
+
+def describe_pet(animal_type, pet_name): 
+    """ Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet("hamster", "harry")
+describe_pet("dog", "willie")
+
+# Keyword Arguments
+  # This is a name-value pair that you pass to a function.
+def describe_pet(animal_type, pet_name): 
+    """ Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(animal_type="hamster", pet_name="harry") #keyword arguments demonstrate that order does not matter
+describe_pet(pet_name="harry", animal_type="hamster")
+
