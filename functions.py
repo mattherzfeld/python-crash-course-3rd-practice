19db372ea1b486dc80c4157b7ecdf4bd953e8984
mattherzfeld <matthew.herzfeld@gmail.com>
2025-04-26 23:54:34 -0500

    Update functions.py
    Default Values

--- functions.py
+++ functions.py
@@ -50,3 +50,11 @@ def describe_pet(animal_type, pet_name):
 describe_pet(animal_type="hamster", pet_name="harry") #keyword arguments demonstrate that order does not matter
 describe_pet(pet_name="harry", animal_type="hamster")
 
+# Default Values
+
+def describe_pet(pet_name, animal_type="dog"):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(pet_name='willie') #animal_type is already defaulted to "dog"
