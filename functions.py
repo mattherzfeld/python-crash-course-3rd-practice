9193685a1c21c85c54786ac9561e471d26fa1423
mattherzfeld <matthew.herzfeld@gmail.com>
2025-04-28 23:35:20 -0500

    Update functions.py
    using a function with a while loop

--- functions.py
+++ functions.py
@@ -151,3 +151,33 @@ another_musician = build_person("paul", "mccartney") #age remains default
 print(musician)
 print(another_musician)
 
+# Using a function with a while loop
+
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+"""THIS IS AN INFINITE LOOP
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
+"""
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q'  at any time to quit)")
+
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
