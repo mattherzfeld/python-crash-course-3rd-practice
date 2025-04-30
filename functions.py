74000b419e51f65fe922a550460a6fdc0dc38faf
mattherzfeld <matthew.herzfeld@gmail.com>
2025-04-30 17:21:03 -0500

    Update functions.py
    working with functions/lists

--- functions.py
+++ functions.py
@@ -237,3 +237,30 @@ while True:
     print(f"\nResults:\n\t{formatted_artist_album}!")
 
 # Passing a List
+
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+
+usernames = ["hannah", "ty", "margot"]
+greet_users(usernames)
+
+# Modifying a List in a Function
+
+#Start with some designs that need to be printed.
+unprinted_designs = ["phone case", "robot pendant", "dodechedron"]
+completed_models = []
+
+#Simulate printing each design until none are left.
+#Move each design to completed_models after printing.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"Printing model: {current_design}")
+    completed_models.append(current_design)
+
+#Display all completed models:
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
