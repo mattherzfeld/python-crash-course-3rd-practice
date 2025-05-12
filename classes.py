f596751d31d8d4f6c969242cd9f6fc79648bb14b
mattherzfeld <matthew.herzfeld@gmail.com>
2025-05-12 00:10:35 -0500

    Update classes.py
    Working with classes/instances

--- classes.py
+++ classes.py
@@ -107,3 +107,22 @@ person_three.greet_user()
 person_three.describe_user()
 person_four.greet_user()
 person_four.describe_user()
+
+
+#Working with Classes and Instances
+
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_descriptive_name(self):
+        """Return a neatly descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
