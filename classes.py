69511cae632c60ec9a92165587386153e676fe2d
mattherzfeld <matthew.herzfeld@gmail.com>
2025-05-12 23:36:14 -0500

    Update classes.py
    Additional methods/attribute value changing.

--- classes.py
+++ classes.py
@@ -118,11 +118,20 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Return a neatly descriptive name."""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+    
+    def read_odometer(self):
+        """Printa  statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
 
 my_new_car = Car('audi', 'a4', 2024)
 print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+
