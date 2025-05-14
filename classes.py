a7cf192c288909e93c328df644c419ef974f41df
mattherzfeld <matthew.herzfeld@gmail.com>
2025-05-14 00:08:52 -0500

    Update classes.py
    working further with a child class

--- classes.py
+++ classes.py
@@ -233,8 +233,17 @@ class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
     def __init__(self, make, model, year):
-        """Initialize attributes of the parent class."""
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
         super().__init__(make, model, year)
+        self.battery_size = 40
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
+my_leaf.describe_battery()
