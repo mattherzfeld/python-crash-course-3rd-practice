9d7d4f9057346d6970a93e2b34970f06e20353da
mattherzfeld <matthew.herzfeld@gmail.com>
2025-05-13 23:57:13 -0500

    Update classes.py
    Inheritance example

--- classes.py
+++ classes.py
@@ -217,7 +217,6 @@ class User:
     def reset_login_attempts(self):
         self.login_attempts = 0
 
-    
 person_five = User("Sven", "Nater", 57, "Gothenburg, Sweden")
 person_five.greet_user()
 person_five.describe_user()
@@ -227,3 +226,15 @@ person_five.increment_login_attempts()
 print(f"Instance has logged in {person_five.login_attempts} times.")
 person_five.reset_login_attempts()
 print(f"Instance has logged in {person_five.login_attempts} times.")
+
+# INHERITANCE
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
