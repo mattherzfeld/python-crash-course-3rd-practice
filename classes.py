e615512e0d525e809f2717f373311b5968a4202c
mattherzfeld <matthew.herzfeld@gmail.com>
2025-05-15 20:58:49 -0500

    Update classes.py
    Overriding methods from the parent class

--- classes.py
+++ classes.py
@@ -244,6 +244,14 @@ class ElectricCar(Car):
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
 
+    def fill_gas_tank(self): # Overriding methods from the parent class
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't have a gas tank!") 
+        #If we did have a fill_gas_tank method in the car class, Python will ignore if that method is called for an electric car.
+
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.describe_battery()
+
+
+
