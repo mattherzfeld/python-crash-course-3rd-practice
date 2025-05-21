c8d668f876ef57001a436297f850aa948b8691c8
mattherzfeld <matthew.herzfeld@gmail.com>
2025-05-20 23:58:18 -0500

    Update file_reader.py
    Example of million pi program.

--- file_reader.py
+++ file_reader.py
@@ -25,3 +25,11 @@ for line in lines:
 
 print(pi_string)
 print(len(pi_string))
+
+
+#An example of an input program - IF we had downloaded the actual million digits of Pi program -
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
