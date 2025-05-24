9424dab1f151fef1c57b84e63b784f4531863e71
mattherzfeld <matthew.herzfeld@gmail.com>
2025-05-23 23:54:28 -0500

    Update exceptions.py
    counting number of words function

--- exceptions.py
+++ exceptions.py
@@ -22,3 +22,28 @@ while True:
         print("You can't divide by 0!")
     else:
         print(answer)
+
+
+# Handling the FileNotFoundError Exception
+
+from pathlib import Path
+
+def count_words(path):
+    """Count the approximate number of words in a file."""
+    try:
+        contents = path.read_text(encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Sorry, the file {path} does not exist.") 
+    else:
+        # Count the approximate number of words in the file:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {path} has about {num_words} words.")
+
+#these are located for free at https://gutenberg.org
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 
+        'little_women.txt']
+for filename in filenames:
+     path = Path(filename)
+     count_words(path)
+
