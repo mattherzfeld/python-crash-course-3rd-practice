55e7a197adc3996eb31c8479ec9f29f45061902e
mattherzfeld <matthew.herzfeld@gmail.com>
2025-05-23 23:29:11 -0500

    Exercise #10-5
    exercise #10-5 completed

--- file_reader.py
+++ file_reader.py
@@ -75,3 +75,16 @@ filename = "guest.txt"
 with open(filename, "w") as f:
     f.write(question)
 
+# 10-5
+
+filename = "guest_book.txt"
+print("Enter 'quit' when you are finished.")
+while True:
+    name = input("\nWhat's your name? ")
+    if name == 'quit':
+        break
+    else:
+        with open(filename, 'a') as f:
+            f.write(name + "\n")
+        print("Hi " + name + ", you've been added to the guest book.")
+
